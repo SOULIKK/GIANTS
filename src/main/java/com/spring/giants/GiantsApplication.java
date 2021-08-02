c2af8a5f22a38e8818e4d1a21ab1a333059021f7
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-08-02 15:38:40 +0900

    create sprintboot project

---
+++ src/main/java/com/spring/giants/GiantsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.giants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiantsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/giants/GiantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.giants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
