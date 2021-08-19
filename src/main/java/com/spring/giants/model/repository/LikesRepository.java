0800df28f6100f13509cbb798105d1e835ea80e1
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-08-19 16:17:31 +0900

    좋아요 여부 - userId, boardId로 체크

---
+++ src/main/java/com/spring/giants/model/repository/LikesRepository.java
@@ -0,0 +1,11 @@
+package com.spring.giants.model.repository;
+
+import com.spring.giants.model.entity.Likes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikesRepository extends JpaRepository<Likes, Long> {
+
+    void deleteByUserIdAndBoardId(Long userId, Long boardId);
+
+    Likes findByUserIdAndBoardId(Long userId, Long boardId);
+}
