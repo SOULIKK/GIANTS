386284bbe8b07b79c5b9a39c83d4a4601fc5dd8d
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-09-05 16:25:16 +0900

    admin dashboard 링크

--- src/main/java/com/spring/giants/controller/MainController.java
+++ src/main/java/com/spring/giants/controller/MainController.java
@@ -1,12 +1,19 @@
 package com.spring.giants.controller;
 
+import com.spring.giants.model.dto.BoardListResponseDto;
+import com.spring.giants.model.dto.BoardRequestDto;
+import com.spring.giants.service.BoardService;
 import com.spring.giants.service.MainService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-
+import java.util.List;
 
 
 @Controller
@@ -14,6 +21,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     final private MainService mainService;
+    final private BoardService boardService;
 
     @GetMapping("/")
     public String main(Model model) {
@@ -35,10 +43,26 @@ public class MainController {
     }
 
     @GetMapping("/main/pick")
-    public String pick(Model model) {
+    public String pick(
+            BoardRequestDto boardRequestDto
+            , @RequestParam(required = false, defaultValue = "") String search
+            , @PageableDefault(size = 10) Pageable pageable
+            , Model model
+    ) {
+
         String state = "pick";
+        String stockId = "EP";
+        Page<BoardListResponseDto> boardListResponseDto = boardService.getBoardList(stockId, search, pageable);
+
+        int startPage = Math.max(1, boardListResponseDto.getPageable().getPageNumber());
+        int endPage = Math.min(boardListResponseDto.getTotalPages(), boardListResponseDto.getPageable().getPageNumber() + 4);
+
+        model.addAttribute("picks", boardListResponseDto);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("state", state);
-        return "/main/pick";
+
+        return "main/pick";
     }
 
 
