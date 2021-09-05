9761b3375399ce9304f64e296e7b56c3b99bed5a
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-09-05 13:28:17 +0900

    admin dashboard 분리

--- src/main/java/com/spring/giants/controller/AdminController.java
+++ src/main/java/com/spring/giants/controller/AdminController.java
@@ -1,7 +1,6 @@
 package com.spring.giants.controller;
 
 
-import com.spring.giants.model.dto.RoleResponseDto;
 import com.spring.giants.model.entity.Role;
 import com.spring.giants.model.entity.User;
 import com.spring.giants.service.UserService;
@@ -22,19 +21,10 @@ public class AdminController {
 
     @GetMapping("/admin/dashboard")
     public String dashboard() {
-        return "user/dashboard";
+        return "admin/dashboard";
     }
 
-    @GetMapping("/main/pick")
-    public String pick(Model model, Authentication authentication) {
-        String state = "pick";
 
-        String username = authentication.getName();
-
-        model.addAttribute("username", username);
-        model.addAttribute("state", state);
-        return "/main/pick";
-    }
 
 
 }
--- src/main/java/com/spring/giants/controller/MainController.java
+++ src/main/java/com/spring/giants/controller/MainController.java
@@ -1,15 +1,13 @@
 package com.spring.giants.controller;
 
-
-import com.spring.giants.model.dto.StockResponseDto;
 import com.spring.giants.service.MainService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+
+
 
 @Controller
 @AllArgsConstructor
