880e3791805460e21beaaf757c0c1b3f66067d7a
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-09-05 16:23:03 +0900

    editor's pick 작성, 리스트 조회

--- src/main/java/com/spring/giants/controller/AdminController.java
+++ src/main/java/com/spring/giants/controller/AdminController.java
@@ -1,16 +1,13 @@
 package com.spring.giants.controller;
 
-
-import com.spring.giants.model.entity.Role;
-import com.spring.giants.model.entity.User;
+import com.spring.giants.model.dto.BoardRequestDto;
+import com.spring.giants.service.BoardService;
 import com.spring.giants.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -18,13 +15,23 @@ import java.util.List;
 public class AdminController {
 
     final private UserService userService;
+    final private BoardService boardService;
 
     @GetMapping("/admin/dashboard")
     public String dashboard() {
         return "admin/dashboard";
     }
 
+    @GetMapping("/admin/write")
+    public String write() {
+        return "admin/write";
+    }
 
-
+    @PostMapping("/admin/submit")
+    public String submit(BoardRequestDto boardRequestDto, Authentication authentication) {
+        String username = authentication.getName();
+        boardService.setBoard(username, boardRequestDto);
+        return "redirect:/main/pick";
+    }
 
 }
