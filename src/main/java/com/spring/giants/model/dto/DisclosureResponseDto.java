33401e49b681eda3fd457fa9d4ea93396bf0d877
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-09-15 17:15:02 +0900

    update disclosure entity, add disclosure dto

--- src/main/java/com/spring/giants/model/dto/DisclosureResponseDto.java
+++ src/main/java/com/spring/giants/model/dto/DisclosureResponseDto.java
@@ -1,2 +1,38 @@
-package com.spring.giants.model.dto;public class DisclosureResponseDto {
+package com.spring.giants.model.dto;
+
+import com.spring.giants.model.entity.Disclosure;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class DisclosureResponseDto {
+
+    private String rcpNo;
+    private String corpCls;
+    private String corpCode;
+    private String corpName;
+    private String flrNm;
+    private String reportNm;
+    private String rceptDt;
+    private String rm;
+    private String stockCode;
+
+    public DisclosureResponseDto(Disclosure disclosure) {
+        this.rcpNo = disclosure.getRcpNo();
+        this.corpCls = disclosure.getCorpCls();
+        this.corpCode = disclosure.getCorpCode();
+        this.corpName = disclosure.getCorpName();
+        this.flrNm = disclosure.getFlrNm();
+        this.reportNm = disclosure.getReportNm();
+        this.rceptDt = disclosure.getRceptDt();
+        this.rm = disclosure.getRm();
+        this.stockCode = disclosure.getStockCode();
+    }
+
 }
--- src/main/java/com/spring/giants/model/entity/Disclosure.java
+++ src/main/java/com/spring/giants/model/entity/Disclosure.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Entity
@@ -22,7 +23,7 @@ public class Disclosure {
     private String corpName;
     private String flrNm;
     private String reportNm;
-    private Date rceptDt;
+    private String rceptDt;
     private String rm;
     private String stockCode;
 
