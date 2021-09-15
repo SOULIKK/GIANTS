351133fd7d4883ffe08d7035cd9b7d248dcb7447
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-09-15 17:16:15 +0900

    set disclosure service, repository

--- src/main/java/com/spring/giants/model/repository/DisclosureRepository.java
+++ src/main/java/com/spring/giants/model/repository/DisclosureRepository.java
@@ -1,2 +1,16 @@
-package com.spring.giants.model.repository;public interface DisclosureRepository {
+package com.spring.giants.model.repository;
+
+import com.spring.giants.model.dto.DisclosureResponseDto;
+import com.spring.giants.model.entity.Disclosure;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public interface DisclosureRepository extends JpaRepository<Disclosure, String> {
+
+
+    Page<DisclosureResponseDto> findByRceptDt(String now_dt, Pageable pageable);
 }
--- src/main/java/com/spring/giants/service/DisclosureService.java
+++ src/main/java/com/spring/giants/service/DisclosureService.java
@@ -1,2 +1,29 @@
-package com.spring.giants.service;public class DisclosureService {
+package com.spring.giants.service;
+
+import com.spring.giants.model.dto.DisclosureResponseDto;
+import com.spring.giants.model.repository.DisclosureRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Service
+public class DisclosureService {
+
+    final private DisclosureRepository disclosureRepository;
+
+    public Page<DisclosureResponseDto> getTodayDisclosure(Pageable pageable) {
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date now = new Date();
+        String now_dt = format.format(now);
+
+        return disclosureRepository.findByRceptDt(now_dt, pageable);
+
+
+    }
 }
