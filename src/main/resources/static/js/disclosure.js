4b7bf2982af2ec10d69cb670f708abc19cb2c776
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-09-15 17:14:08 +0900

    add today disclosure template

---
+++ src/main/resources/static/js/disclosure.js
@@ -0,0 +1,4 @@
+function reportLink(rcpNo) {
+    const link = 'http://dart.fss.or.kr/dsaf001/main.do?rcpNo='+rcpNo;
+    window.open(link, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=50,left=50,width=1024,height=768");
+}
