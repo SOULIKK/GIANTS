fbf63d9c1aa926e1f340cd2efab06a5f15f05b81
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-09-21 14:28:48 +0900

    FEAT: Add search condition search by date

--- src/main/resources/static/js/disclosure.js
+++ src/main/resources/static/js/disclosure.js
@@ -2,3 +2,19 @@ function reportLink(rcpNo) {
     const link = 'http://dart.fss.or.kr/dsaf001/main.do?rcpNo='+rcpNo;
     window.open(link, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=50,left=50,width=1024,height=768");
 }
+
+
+const today = new Date().toISOString().slice(0, 10);
+
+document.getElementById("searchEnd").value = today;
+document.getElementById("searchEnd").value = today;
+
+function searchValidation() {
+    const searchStart = document.getElementById("searchStart").value;
+    const searchEnd = document.getElementById("searchEnd").value;
+
+    if (searchStart == "" || searchEnd == "") {
+        alert("검색일자를 입력해주세요");
+        return false;
+    }
+}
