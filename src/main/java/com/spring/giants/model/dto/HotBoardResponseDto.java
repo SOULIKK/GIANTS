0e26148d97915ae7ad0ebbd052227f449854a90f
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-10-31 18:26:07 +0900

    FEAT: Add hot board list

---
+++ src/main/java/com/spring/giants/model/dto/HotBoardResponseDto.java
@@ -0,0 +1,37 @@
+package com.spring.giants.model.dto;
+
+import com.spring.giants.model.entity.Board;
+import com.spring.giants.model.entity.User;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class HotBoardResponseDto {
+
+    private Long boardId;
+    private String title;
+    private String content;
+    private User user;
+    private String stockId;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    private Long commentCnt;
+    private Long likesCnt;
+
+    public HotBoardResponseDto(Board board, Long commentCnt, Long likesCnt) {
+        this.boardId = board.getBoardId();
+        this.title = board.getTitle();
+        this.content = board.getContent();
+        this.user = board.getUser();
+        this.stockId = board.getStockId();
+        this.createdAt = board.getCreatedAt();
+        this.modifiedAt = board.getModifiedAt();
+        this.commentCnt = commentCnt;
+        this.likesCnt = likesCnt;
+
+    }
+}
