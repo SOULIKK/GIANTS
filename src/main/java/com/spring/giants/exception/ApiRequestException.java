7d3728ebeffec98b41e0a970ac5839efe254929a
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-12-12 23:42:13 +0900

    FEAT: Add exception

---
+++ src/main/java/com/spring/giants/exception/ApiRequestException.java
@@ -0,0 +1,17 @@
+package com.spring.giants.exception;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+public class ApiRequestException extends IllegalArgumentException {
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
