a9f761e6c174d4be8499e6ff8ba22a58a220dc24
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2022-01-03 23:33:43 +0900

    REFACTOR: Rename rest controller, add cert send key, check cert key

--- src/main/java/com/spring/giants/controller/RestApiController.java
+++ src/main/java/com/spring/giants/controller/RestApiController.java
@@ -2,6 +2,7 @@ package com.spring.giants.controller;
 
 import com.spring.giants.model.dto.CommentResponseDto;
 import com.spring.giants.service.CommentService;
+import com.spring.giants.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +12,10 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-public class RestCommentController {
+public class RestApiController {
 
     private final CommentService commentService;
+    private final UserService userService;
 
 
     @PostMapping("/comment/ep")
@@ -31,6 +33,20 @@ public class RestCommentController {
 
         return new ResponseEntity<>(commentResponseDtoList, HttpStatus.OK);
     }
+
+    @PostMapping("/cert/send")
+    public ResponseEntity<String> sendMail(@RequestParam String email) {
+        System.out.println("emailemailemailemailemail = " + email);
+        userService.sendMail(email);
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+    @PostMapping("/cert/check")
+    public ResponseEntity<Boolean> chkCert(@RequestParam String email, @RequestParam int certKey) {
+        Boolean chkCert = userService.chkCert(email, certKey);
+        return new ResponseEntity<>(chkCert, HttpStatus.OK);
+    }
+
 
 
 }
