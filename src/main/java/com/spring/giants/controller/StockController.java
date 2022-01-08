16a2e9f99101cb53d05b04c9bdfd6dd8fedfd4db
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2022-01-08 17:06:36 +0900

    REFACTOR: Add search result page (list type)

--- src/main/java/com/spring/giants/controller/StockController.java
+++ src/main/java/com/spring/giants/controller/StockController.java
@@ -4,17 +4,13 @@ package com.spring.giants.controller;
 import com.spring.giants.model.dto.BoardListResponseDto;
 import com.spring.giants.model.dto.DisclosureResponseDto;
 import com.spring.giants.model.dto.StockDto;
-import com.spring.giants.model.entity.Stock;
 import com.spring.giants.service.BoardService;
 import com.spring.giants.service.DisclosureService;
 import com.spring.giants.service.MainService;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
 
 
 import java.util.List;
@@ -27,19 +23,28 @@ public class StockController {
     private final BoardService boardService;
     private final DisclosureService disclosureService;
 
-
     @GetMapping("/main/stock")
     public String searchedValue(String stockName, Model model) {
 
-        Stock stock = mainService.getStockByStockName(stockName);
+        List<StockDto> stockDto = mainService.getStockByStockName(stockName);
 
-        List<BoardListResponseDto> boardListResponseDto = boardService.getMainBoardList(stock);
-        List<DisclosureResponseDto> disclosureResponseDto = disclosureService.getReport(stock.getStockId());
+        if (stockDto.size() == 1) {
+            StockDto stockDto1 = new StockDto(stockDto.get(0).getStockId(), stockDto.get(0).getStockName(), stockDto.get(0).getMarket(), stockDto.get(0).getSector(), stockDto.get(0).getProduct(), stockDto.get(0).getHomepage());
+            List<BoardListResponseDto> boardListResponseDto = boardService.getMainBoardList(stockDto.get(0));
+            List<DisclosureResponseDto> disclosureResponseDto = disclosureService.getReport(stockDto.get(0).getStockId());
 
-        model.addAttribute("stock", stock);
-        model.addAttribute("boards", boardListResponseDto);
-        model.addAttribute("reports", disclosureResponseDto);
+            model.addAttribute("stock", stockDto1);
+            model.addAttribute("boards", boardListResponseDto);
+            model.addAttribute("reports", disclosureResponseDto);
 
-        return "main/stock";
+            return "main/stock";
+        }
+        if (stockDto.isEmpty()) {
+            model.addAttribute("key", stockName);
+            model.addAttribute("stockList", "none");
+            return "main/searchedPage";
+        }
+        model.addAttribute("stockList", stockDto);
+        return "main/searchedPage";
     }
 }
