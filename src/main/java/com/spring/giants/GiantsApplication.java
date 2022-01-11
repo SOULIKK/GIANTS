c73cd118e2a1a019ab0c986beffa8d5703a220bf
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2022-01-11 23:06:55 +0900

    REFACTOR: Set asia timezone, cors

--- src/main/java/com/spring/giants/GiantsApplication.java
+++ src/main/java/com/spring/giants/GiantsApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class GiantsApplication {
 
+    @PostConstruct
+    public void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(GiantsApplication.class, args);
     }
